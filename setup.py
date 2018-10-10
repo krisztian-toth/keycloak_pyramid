b0964a7064f34374c6d8bd3e347443a6f379abc5
KrisztianToth <krisztian.toth@labela.nl>
2018-10-10 12:26:40 +0200

    add long_description and URL to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="pyramid_keycloak",
     version="0.0.13",
     author="Krisztian Toth",
     author_email="tkrisztiana@gmail.com",
     description="Keycloak based authentication policy for Pyramid",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/watsta/keycloak_pyramid",
     packages=setuptools.find_packages(),
     install_requires=[
         'pyramid>1.0,<2.0',
